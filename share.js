fa2e3a87a370687973a5b9cfad7c6d4edf3e093b
Pavel Arapov <pavel.arapov@gmail.com>
2012-06-14 17:18:51 +0200

    sync save document test

--- share.js
+++ share.js
@@ -105,24 +105,24 @@ var options = {db: {type: 'none'}}; // See docs for options. {type: 'redis'} to 
 // Attach the sharejs REST and Socket.io interfaces to the server
 sharejs.attach(app, options);
 
-var client = require('share').client;
-
-client.open('4fd5dbf585dcbff208000001', 'text', {host: config.host_sync_uri, port: config.port_sharejs}, function(doc, error) {
-    // Insert some text at the start of the document (position 0):
-//    doc.submitOp({i:"Hi there!\n", p:0});
-
-    // Get the contents of the document for some reason:
-//    console.log(doc.snapshot);
-
-    doc.on('change', function(op) {
-        var data = [];
-        data['_id'] = '4fd5dbf585dcbff208000001';
-        data['article'] = doc.snapshot;
-        dao.pages.update(data, function (error, result) {
-            if (error != undefined)
-                console.log("Got an error: " + error);
-
-        });
-//        console.log('Version: ' + doc.version);
-    });
-});
+//var client = require('share').client;
+//
+//client.open('4fd5dbf585dcbff208000001', 'text', {host: config.host_sync_uri, port: config.port_sharejs}, function(doc, error) {
+//    // Insert some text at the start of the document (position 0):
+////    doc.submitOp({i:"Hi there!\n", p:0});
+//
+//    // Get the contents of the document for some reason:
+////    console.log(doc.snapshot);
+//
+//    doc.on('change', function(op) {
+//        var data = [];
+//        data['_id'] = '4fd5dbf585dcbff208000001';
+//        data['article'] = doc.snapshot;
+//        dao.pages.update(data, function (error, result) {
+//            if (error != undefined)
+//                console.log("Got an error: " + error);
+//
+//        });
+////        console.log('Version: ' + doc.version);
+//    });
+//});
