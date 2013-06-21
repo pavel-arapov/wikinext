7000e761dc165462269d3deb42e382b0851f8c29
Pavel Arapov <pavel.arapov@gmail.com>
2013-06-21 09:30:38 +0200

    WN-29, load a type complete

--- routes/index.js
+++ routes/index.js
@@ -137,8 +137,8 @@ function getSchemaType(type){
     var response = schema_org_cache['types'][type];
     for (var prop in response['properties']) {
         var property = response['properties'][prop];
-        //console.log(response['properties'][prop]);
-        //console.log(schema_org_cache['properties'][response['properties'][prop]]);
+        console.log(property);
+        console.log(schema_org_cache['properties'][property]);
         response['properties'][prop] = schema_org_cache['properties'][property];
     }
     return response;
